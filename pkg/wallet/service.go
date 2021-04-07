5b5534e54ebba23728c0ed5afac41976831a7618
Daler <>
2021-04-08 02:36:34 +0300

    added package wallet

---
+++ pkg/wallet/service.go
@@ -0,0 +1,25 @@
+package wallet
+
+import (
+	"github.com/Kamolov-Daler/wallet/pkg/types"
+)
+
+type Service struct {
+	nextAccountID int64
+	accounts      []types.Account
+	payments      []types.Payment
+}
+
+func RegisterAccount(service *Service, phone types.Phone) {
+	for _, account := range service.accounts {
+		if account.Phone == phone {
+			return
+		}
+	}
+	service.nextAccountID++
+	service.accounts = append(service.accounts, types.Account{
+		ID:      service.nextAccountID,
+		Phone:   phone,
+		Balance: 0,
+	})
+}
